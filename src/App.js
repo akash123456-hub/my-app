e2fe125d79732e3886d3532db6058552ed3701e6
akash0542 <akash.m@ipsator.com>
2023-04-25 22:18:07 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert  from './components/Alert';
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message,type) =>{
+    setAlert({
+      message:message,
+      type:type
+    })
+
+  }
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor = 'grey';
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","success");
+      
     }
     else {
       setMode('light')
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
     }
 
 
@@ -23,6 +35,7 @@ function App() {
     <>
       {/* <Navbar title="My-App" aboutApp="About App"/> */}
       <Navbar title="MyApp" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
         {/* <TextForm /> */}
       </div>
